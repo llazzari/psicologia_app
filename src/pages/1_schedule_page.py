77d345bce07bbfc372dbd25c14343942a6ef1a5d
llazzari <lucas.lazzari@outlook.com>
2025-06-18 13:56:47 -0300

    refactor: add type ignore

--- src/pages/1_schedule_page.py
+++ src/pages/1_schedule_page.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import pandas as pd
 import streamlit as st
 
-from utils.helpers import generate_time_slots, get_week_days
+from utils.helpers import generate_time_slots, get_week_days  # type: ignore
 
 st.set_page_config(layout="wide")
 st.title("🗓️ Agendamento Semanal")
@@ -23,7 +23,7 @@ with col3:
         st.rerun()
 
 # --- Calendar Grid ---
-week_days = get_week_days(st.session_state.current_date)
+week_days = get_week_days(st.session_state.current_date)  # type: ignore
 morning_time_slots = generate_time_slots(
     start_hour=8, start_minutes=0, end_hour=11, end_minutes=45, interval_minutes=45
 )
@@ -34,12 +34,12 @@ time_slots = morning_time_slots + afternoon_time_slots
 
 days_of_week_pt = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta"]
 days_of_week = [
-    f"{pt_day} ({day.strftime('%d/%m')})"
-    for day, pt_day in zip(week_days, days_of_week_pt)
+    f"{pt_day} ({day.strftime('%d/%m')})"  # type: ignore
+    for day, pt_day in zip(week_days, days_of_week_pt)  # type: ignore
 ]
 time_labels = [slot.strftime("%H:%M") for slot in time_slots]
 data = {day: [""] * len(time_slots) for day in days_of_week}
 df = pd.DataFrame(data, index=time_labels)
 df.index.name = "Horário"
 
-st.dataframe(df, use_container_width=True, height=600)
+st.dataframe(df, use_container_width=True, height=600)  # type: ignore
