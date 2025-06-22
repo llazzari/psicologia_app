a5722ca1830113a621b51f187160ef61b5fe576e
llazzari <lucas.lazzari@outlook.com>
2025-06-22 13:59:00 -0300

    feat: show appointments dataframe in st.data_editor format

--- src/pages/1_schedule_page.py
+++ src/pages/1_schedule_page.py
@@ -3,7 +3,9 @@ from datetime import datetime
 import pandas as pd
 import streamlit as st
 
-from utils.helpers import generate_time_slots, get_week_days  # type: ignore
+from data import appointment, database
+from data.database import DB_PATH
+from utils.helpers import get_week_days  # type: ignore
 
 st.set_page_config(layout="wide")
 st.title("🗓️ Agendamento Semanal")
@@ -11,8 +13,8 @@ st.title("🗓️ Agendamento Semanal")
 if "current_date" not in st.session_state:
     st.session_state.current_date = datetime.today().date()
 
-col1, col2, col3 = st.columns([1, 2, 1])
-
+# !FIXME Align col3 with the right side of the page
+col1, col3 = st.columns([1, 1])
 with col1:
     if st.button("⬅️ Semana Anterior"):
         st.session_state.current_date -= pd.Timedelta(weeks=1)
@@ -22,24 +24,10 @@ with col3:
         st.session_state.current_date += pd.Timedelta(weeks=1)
         st.rerun()
 
-# --- Calendar Grid ---
 week_days = get_week_days(st.session_state.current_date)  # type: ignore
-morning_time_slots = generate_time_slots(
-    start_hour=8, start_minutes=0, end_hour=11, end_minutes=45, interval_minutes=45
-)
-afternoon_time_slots = generate_time_slots(
-    start_hour=13, start_minutes=30, end_hour=19, end_minutes=30, interval_minutes=45
-)
-time_slots = morning_time_slots + afternoon_time_slots
 
-days_of_week_pt = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta"]
-days_of_week = [
-    f"{pt_day} ({day.strftime('%d/%m')})"  # type: ignore
-    for day, pt_day in zip(week_days, days_of_week_pt)  # type: ignore
-]
-time_labels = [slot.strftime("%H:%M") for slot in time_slots]
-data = {day: [""] * len(time_slots) for day in days_of_week}
-df = pd.DataFrame(data, index=time_labels)
-df.index.name = "Horário"
+with database.connect(DB_PATH) as connection:
+    appointments: pd.DataFrame = appointment.list_all_for_week(connection, week_days)  # type: ignore
 
-st.dataframe(df, use_container_width=True, height=600)  # type: ignore
+
+updated_df = st.data_editor(appointments, use_container_width=True)  # type: ignore
