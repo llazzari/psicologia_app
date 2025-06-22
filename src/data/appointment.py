962067d90fffb30c098ef1d7838c44b14fbb62ea
llazzari <lucas.lazzari@outlook.com>
2025-06-22 13:56:47 -0300

    fix: correct the appointments dataframe that are retrieved from list_all_for_week function

--- src/data/appointment.py
+++ src/data/appointment.py
@@ -6,6 +6,7 @@ import duckdb
 import pandas as pd
 
 from data.models import Appointment
+from utils.helpers import generate_time_slots
 
 log = logging.getLogger("TestLogger")
 
@@ -64,7 +65,7 @@ def get_by_id(
     connection: duckdb.DuckDBPyConnection, appointment_id: UUID
 ) -> Appointment:
     """
-    Retrieves a appointment by ID from the 'appointments' table.
+    Retrieves an appointment by ID from the 'appointments' table.
     Returns a Pydantic model instance.
     """
     try:
@@ -145,20 +146,41 @@ def list_all_for_week(
         """
         cursor = connection.execute(sql, (min(week), max(week)))
         df: pd.DataFrame = cursor.df()
+
+        days = [
+            f"{['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta'][d.weekday()]} ({d.day:02d}/{d.month:02d})"
+            for d in week
+        ]
+
+        morning_time_slots = generate_time_slots(
+            start_hour=8,
+            start_minutes=0,
+            end_hour=11,
+            end_minutes=45,
+            interval_minutes=45,
+        )
+        afternoon_time_slots = generate_time_slots(
+            start_hour=13,
+            start_minutes=30,
+            end_hour=19,
+            end_minutes=30,
+            interval_minutes=45,
+        )
+        time_slots = morning_time_slots + afternoon_time_slots
+
         if df.empty:
             log.warning("No appointments found.")
             # Create an empty DataFrame with the correct structure
-            days = [
-                f"{['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta'][d.weekday()]} ({d.day:02d}/{d.month:02d})"
-                for d in week
-            ]
+
             df = pd.DataFrame(columns=days)
         else:
             # Pivot the DataFrame so that each weekday is a column
             df = df.pivot(
                 index="appointment_hour", columns="day_column", values="patient_name"
-            )
+            ).reindex(index=time_slots, columns=days, fill_value="")
         df.index.name = "Horário"
+        df.index = pd.to_datetime(df.index.astype(str)).strftime("%H:%M")  # type: ignore
+        df.fillna("", inplace=True)  # type: ignore
         log.info("Successfully retrieved appointments schedule for the week.")
         return df
     except Exception:
