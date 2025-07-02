e184e9e2da60839f165bd995a707fd6e553e95d8
llazzari <lucas.lazzari@outlook.com>
2025-07-02 13:30:22 -0300

    feat: enhance monthly invoice handling with new retrieval and insertion logic

--- src/data/monthly_invoice.py
+++ src/data/monthly_invoice.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 from uuid import UUID
 
@@ -19,10 +20,13 @@ def create_monthly_invoices_table(connection: duckdb.DuckDBPyConnection) -> None
         CREATE TABLE IF NOT EXISTS monthly_invoices (
             id UUID PRIMARY KEY, 
             patient_id UUID NOT NULL,
-            invoice_month VARCHAR NOT NULL,
+            invoice_month INTEGER NOT NULL,
+            invoice_year INTEGER NOT NULL,
             session_price INTEGER NOT NULL,
             sessions_completed INTEGER NOT NULL,
+            sessions_to_recover INTEGER NOT NULL,
             payment_status VARCHAR CHECK (payment_status IN ('pending', 'paid', 'overdue', 'waived')) NOT NULL,
+            nf_number INTEGER,
             payment_date DATE
         );
         """
@@ -35,7 +39,7 @@ def create_monthly_invoices_table(connection: duckdb.DuckDBPyConnection) -> None
         raise
 
 
-def add(connection: duckdb.DuckDBPyConnection, invoice: MonthlyInvoice) -> UUID:
+def insert(connection: duckdb.DuckDBPyConnection, invoice: MonthlyInvoice) -> UUID:
     """
     Adds a new monthly invoice to the 'monthly_invoices' table.
     """
@@ -47,8 +51,19 @@ def add(connection: duckdb.DuckDBPyConnection, invoice: MonthlyInvoice) -> UUID:
         connection.register("invoice_df", invoice_df)
 
         sql = """
-        INSERT INTO monthly_invoices 
-        SELECT id, patient_id, invoice_month, session_price, sessions_completed, payment_status, payment_date FROM invoice_df
+        INSERT OR REPLACE INTO monthly_invoices 
+        SELECT 
+            id, 
+            patient_id, 
+            invoice_month, 
+            invoice_year, 
+            session_price, 
+            sessions_completed, 
+            sessions_to_recover, 
+            payment_status, 
+            nf_number, 
+            payment_date
+        FROM invoice_df
         """
         connection.execute(sql)
         log.info(
@@ -96,27 +111,85 @@ def get_by_id(
         raise
 
 
-def update(connection: duckdb.DuckDBPyConnection, invoice: MonthlyInvoice) -> None:
+def get_all_in_period(
+    connection: duckdb.DuckDBPyConnection, month: int, year: int
+) -> list[MonthlyInvoice]:
     """
-    Updates an existing monthly invoice's data in the 'monthly_invoices' table.
+    Retrieves all monthly invoices for a given month from the 'monthly_invoices' table.
+    Returns a list of Pydantic model instances.
     """
-    log.info(f"APP-LOGIC: Attempting to update monthly invoice with ID {invoice.id}.")
-    invoice_df = pd.DataFrame([invoice.model_dump()])
+    try:
+        log.info(
+            f"APP-LOGIC: Attempting to retrieve all invoices for month {month} and year {year}."
+        )
+        month_begin: datetime.date = datetime.date(year, month, 1)
+        month_end: datetime.date = month_begin + datetime.timedelta(days=31)
+        sql = """
+            SELECT
+                patient_id,
+                -- Conta condicionalmente as sessões com status 'done'
+                COUNT(CASE WHEN status = 'done' THEN 1 END) AS sessions_completed,
+                -- Conta condicionalmente as sessões com status 'to recover'
+                COUNT(CASE WHEN status = 'to recover' THEN 1 END) AS sessions_to_recover,
+                 -- CONTA SEPARADAMENTE AS SESSÕES GRATUITAS
+                COUNT(CASE WHEN is_free_of_charge = true THEN 1 END) AS free_sessions_count,
+                -- Agrega todas as datas de agendamento feitos em uma lista
+                COALESCE(
+                    list(appointment_date ORDER BY appointment_date ASC) FILTER (WHERE status = 'done'),
+                    []
+                ) AS completed_appointment_dates
+            FROM
+                appointments
+            WHERE
+                -- Filtra os agendamentos para um mês e ano específicos (ex: Junho de 2025)
+                appointment_date BETWEEN ? AND ?
+            GROUP BY
+                patient_id;
+        """
+        results = connection.execute(sql, (month_begin, month_end)).fetchall()  # type: ignore
 
-    connection.register("invoice_df", invoice_df)
+        if not results:
+            log.warning(
+                f"APP-LOGIC: No invoices found for month {month} and year {year}."
+            )
+            return []
 
-    sql = """
-    UPDATE monthly_invoices 
-    SET patient_id = invoice_df.patient_id, 
-        invoice_month = invoice_df.invoice_month, 
-        session_price = invoice_df.session_price, 
-        sessions_completed = invoice_df.sessions_completed, 
-        payment_status = invoice_df.payment_status, 
-        payment_date = invoice_df.payment_date
-    FROM invoice_df
-    WHERE monthly_invoices.id = invoice_df.id;
-    """
+        invoices: list[MonthlyInvoice] = []
 
-    connection.execute(sql)
+        for row in results:
+            patient_id = row[0]
+            sessions_completed = row[1]
+            sessions_to_recover = row[2]
+            free_sessions = row[3]
+            appointment_dates = [date for date in row[4] if date is not None]
 
-    log.info(f"APP-LOGIC: Successfully updated monthly invoice with ID {invoice.id}.")
+            if not appointment_dates:
+                log.warning(
+                    f"APP-LOGIC: No appointments found for patient ID {patient_id} in month {month} and year {year}."
+                )
+                continue
+
+            new_invoice = MonthlyInvoice(
+                patient_id=patient_id,
+                invoice_month=month,
+                invoice_year=year,
+                appointment_dates=appointment_dates,
+                sessions_completed=sessions_completed,
+                sessions_to_recover=sessions_to_recover,
+                free_sessions=free_sessions,
+            )
+
+            insert(connection, new_invoice)
+
+            invoices.append(new_invoice)
+
+        log.info(
+            f"APP-LOGIC: Successfully retrieved {len(invoices)} invoices for month {month} and year {year}."
+        )
+        return invoices
+    except Exception:
+        log.error(
+            f"APP-LOGIC: Failed to retrieve invoices for month {month} and year {year}.",
+            exc_info=True,
+        )
+        raise
