068c11ad6de81098fdda63e44d43c58ee0e7ca75
llazzari <lucas.lazzari@outlook.com>
2025-07-04 17:18:48 -0300

    feat: enhance sidebar navigation with user login section and icons

--- src/modules/navbar.py
+++ src/modules/navbar.py
@@ -2,8 +2,27 @@ import streamlit as st
 
 
 def render() -> None:
+    st.logo(
+        "assets/marca_roxa1.png", icon_image="assets/simbolo_roxo1.png", size="large"
+    )
     with st.sidebar:
-        st.page_link("main.py", label="Página Inicial")
-        st.page_link("pages/2_patients_page.py", label="Pacientes")
-        st.page_link("pages/1_schedule_page.py", label="Agendamento Semanal")
-        st.page_link("pages/3_monthly_invoices.py", label="Controle Financeiro")
+        st.markdown("## Usuário Logado")
+        st.page_link("pages/logout.py", label="Logout", icon=":material/logout:")
+
+        st.markdown("## Navegação")
+        st.page_link("main.py", label="Página Inicial", icon=":material/home:")
+        st.page_link(
+            "pages/2_patients_page.py",
+            label="Pacientes",
+            icon=":material/patient_list:",
+        )
+        st.page_link(
+            "pages/1_schedule_page.py",
+            label="Agendamento Semanal",
+            icon=":material/event:",
+        )
+        st.page_link(
+            "pages/3_monthly_invoices.py",
+            label="Controle Financeiro",
+            icon=":material/attach_money:",
+        )
