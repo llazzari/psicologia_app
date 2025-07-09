b0ca15c3ff1a43d3d8bdbc543d0cfa85292bae92
llazzari <lucas.lazzari@outlook.com>
2025-07-09 11:13:46 -0300

    feat: add caching for patient retrieval by ID to improve performance

--- src/service/patient_manager.py
+++ src/service/patient_manager.py
@@ -1,4 +1,7 @@
 from typing import Optional
+from uuid import UUID
+
+import streamlit as st
 
 from data import patient
 from data.models import Patient
@@ -15,3 +18,9 @@ def get_all_patients(
 ) -> list[Patient]:
     connection = get_db_connection()
     return patient.get_all(connection, are_active, status)
+
+
+@st.cache_data(ttl=3600)
+def get_patient_by_id(patient_id: UUID) -> Patient:
+    connection = get_db_connection()
+    return patient.get_by_id(connection, patient_id)
