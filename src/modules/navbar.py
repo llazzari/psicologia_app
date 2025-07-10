fb504f01c77def0c150960af9af8bbc2d9e239d9
llazzari <lucas.lazzari@outlook.com>
2025-07-10 18:23:25 -0300

    fix: update navigation links and improve sidebar structure

--- src/modules/navbar.py
+++ src/modules/navbar.py
@@ -6,23 +6,25 @@ def render() -> None:
         "assets/marca_roxa1.png", icon_image="assets/simbolo_roxo1.png", size="large"
     )
     with st.sidebar:
+        st.page_link("main.py", label="Página Inicial", icon=":material/home:")
+
         st.markdown("## Usuário Logado")
         st.page_link("pages/logout.py", label="Logout", icon=":material/logout:")
 
-        st.markdown("## Navegação")
-        st.page_link("main.py", label="Página Inicial", icon=":material/home:")
+        st.markdown("## Gerenciamento")
+
         st.page_link(
-            "pages/2_patients_page.py",
+            "pages/patients_page.py",
             label="Pacientes",
             icon=":material/patient_list:",
         )
         st.page_link(
-            "pages/1_schedule_page.py",
+            "pages/schedule_page.py",
             label="Agendamento Semanal",
             icon=":material/event:",
         )
         st.page_link(
-            "pages/3_monthly_invoices.py",
+            "pages/monthly_invoices.py",
             label="Controle Financeiro",
             icon=":material/attach_money:",
         )
