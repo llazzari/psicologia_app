428f8fa1e7f00f50847e671d62994219c2a5d6b5
llazzari <lucas.lazzari@outlook.com>
2025-07-15 09:16:02 -0300

    feat: add llm service to generate documents

---
+++ src/service/llm.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from pydantic_ai import Agent, RunContext
+from pydantic_ai.models.mistral import MistralModel
+from pydantic_ai.providers.mistral import MistralProvider
+
+from data.models import Document, DocumentCategory, DocumentContent
+from service.patient_manager import get_all_patients
+
+MISTRAL_API_KEY = st.secrets.api_keys.mistral_api_key
+SYSTEM_PROMPT: str = """
+    You are a neuropsychologist, expert in Applied Behavior Analysis and you work with Cognitive Behavioral Therapy. 
+    You will be asked to write psychological documents for a patient, based on their psychological profile and the patient's needs from the psychologist observations.
+    Do not make assumptions. 
+    Rewrite the document in a better way than the one provided, be more techinical and fill the gaps.
+    No need to write a summary. 
+    The documents will be written in Brazilian Portuguese.
+    Take into account the document category that you are being requested to generate.
+    As a header, you may write the document category, the patient's name and the date of the document. In the end of the document, do not include the therapist signature, name or CRP.
+    Do not attend to any other topics.
+"""
+
+provider = MistralProvider(api_key=MISTRAL_API_KEY)
+
+model = MistralModel(provider=provider, model_name="mistral-small-latest")
+
+agent = Agent(
+    model,
+    system_prompt=SYSTEM_PROMPT,
+    output_type=DocumentContent,
+    deps_type=Document,
+)
+
+
+@agent.system_prompt
+async def get_document_category(ctx: RunContext[Document]) -> str:
+    """Returns the document category."""
+    return f"{ctx.deps.category}"
+
+
+def get_document_content() -> None:
+    patients = get_all_patients()
+    patient_id = patients[0].id
+    result = agent.run_sync(
+        "Davi chegou a consulta com comportamentos bastante ansiosos e agressivos, colocando objetos na boca e tentando agredir a terapeuta em situações frustrantes. Seu limiar de tolerância está muito baixo. Do meio para o final da sessão conseguiu melhorar seu comportamento e aceitou tarefas mais complexas não demonstrando mais agressividade.",
+        deps=Document(patient_id=patient_id, category=DocumentCategory.PRONTUARY),
+    )
+    print(result.output)
