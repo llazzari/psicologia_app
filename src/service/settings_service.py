648855bfa7db77522bdf97c0bec8cdc82a02c8ac
llazzari <lucas.lazzari@outlook.com>
2025-07-17 09:17:01 -0300

    refactor: rename save function to insert for clarity and update import path for database connection

--- src/service/settings_service.py
+++ src/service/settings_service.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from database_manager import get_db_connection
 
 from data import psychologist_settings
 from data.models import PsychologistSettings
+from service.database_manager import get_db_connection
 
 
 @st.cache_data(ttl=3600)
@@ -11,6 +11,6 @@ def get_by_(email: str) -> PsychologistSettings:
     return psychologist_settings.get_by_email(connection, email)
 
 
-def save(settings: PsychologistSettings) -> None:
+def insert(settings: PsychologistSettings) -> None:
     connection = get_db_connection()
     psychologist_settings.insert(connection, settings)
