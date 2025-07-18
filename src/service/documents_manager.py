50fb45fbd0dae98cb9b686cc2e3c2db62036c8c4
llazzari <lucas.lazzari@outlook.com>
2025-07-18 16:49:56 -0300

    refactor: rename get_all_documents to get_all_documents_for for improved clarity in document retrieval

--- src/service/documents_manager.py
+++ src/service/documents_manager.py
@@ -5,7 +5,7 @@ from data.models import Document, DocumentCategory
 from service.database_manager import get_db_connection
 
 
-def get_all_documents(patient_id: UUID) -> list[Document]:
+def get_all_documents_for(patient_id: UUID) -> list[Document]:
     connection = get_db_connection()
     return documents.get_all_for_patient_id(connection, patient_id)
 
@@ -19,7 +19,7 @@ def _get_all_for_category(
 def get_all_for_category(
     patient_id: UUID, category: DocumentCategory
 ) -> list[Document]:
-    all_documents = get_all_documents(patient_id)
+    all_documents = get_all_documents_for(patient_id)
     return _get_all_for_category(all_documents, category)
 
 
