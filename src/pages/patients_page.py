e01de8f52b18d84bb69f8b52a3fdf4e9b1e755ae
llazzari <lucas.lazzari@outlook.com>
2025-07-18 16:49:45 -0300

    feat: integrate asynchronous document editing and patient retrieval in patients page

--- src/pages/patients_page.py
+++ src/pages/patients_page.py
@@ -1,3 +1,4 @@
+import asyncio
 import uuid
 
 import streamlit as st
@@ -5,19 +6,26 @@ import streamlit as st
 from modules import navbar
 from pages import patients_list
 from pages.documents_view import display_patient_docs, doc_editor
+from service.patient_manager import get_patient_by_id
 
 
 def main() -> None:
     st.set_page_config(
-        layout="wide", page_title="Pacientes", initial_sidebar_state="collapsed", page_icon=":material/network_intelligence:",
+        layout="wide",
+        page_title="Pacientes",
+        initial_sidebar_state="collapsed",
+        page_icon=":material/network_intelligence:",
     )
     navbar.render()
 
-    if "patient_id" in st.query_params:
-        if "edit" in st.query_params:
-            doc_editor()
-            return
+    if "edit" in st.query_params:
+        asyncio.run(doc_editor())
+    elif "patient_id" in st.query_params:
+        st.session_state["patient"] = get_patient_by_id(
+            uuid.UUID(st.query_params["patient_id"])
+        )
         display_patient_docs(uuid.UUID(st.query_params["patient_id"]))
+
     else:
         patients_list.render()
 
