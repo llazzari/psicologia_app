497e2b7f669319ad0e4b2e47ba30a52d6705424c
llazzari <lucas.lazzari@outlook.com>
2025-07-18 16:49:36 -0300

    feat: enhance document editing interface with AI content generation and improved state management

--- src/pages/documents_view.py
+++ src/pages/documents_view.py
@@ -1,26 +1,28 @@
-from typing import Optional
 from uuid import UUID
 
+import numpy as np
 import streamlit as st
 
-from data.models import DOCUMENT_CATEGORY_PT, Document, DocumentCategory
-from service.documents_manager import get_all_for_category, insert_document
+from data.models import (
+    DOCUMENT_CATEGORY_PT,
+    Document,
+    DocumentCategory,
+    DocumentContent,
+    Patient,
+)
+from service import llm
+from service.documents_manager import get_all_documents_for, insert_document
 from service.patient_manager import get_patient_by_id
 
 
-def doc_editor(doc: Optional[Document] = None) -> None:
-    patient_ = st.session_state["patient"]
-    if not doc:
-        doc = Document(patient_id=patient_.id)
+async def doc_editor() -> None:
+    doc = st.session_state["doc"]
 
     st.title("Editar documento")
-    if st.button("Voltar a lista de documentos", icon=":material/arrow_back:"):
-        st.query_params.pop("edit")
-        st.rerun()
 
     cols = st.columns([3, 1])
     with cols[0]:
-        doc.file_name = st.text_input("Nome do documento")
+        doc.file_name = st.text_input("Nome do documento", value=doc.file_name)
     with cols[1]:
         doc.category = st.selectbox(
             "Categoria",
@@ -31,40 +33,55 @@ def doc_editor(doc: Optional[Document] = None) -> None:
 
     st.divider()
 
-    col_input, col_btn, col_output = st.columns([3, 1, 3], vertical_alignment="center")
-    with col_input:
-        st.text_area(
-            "Observações",
-            key=f"observations_{patient_.id}",
-            height=200,
+    col_obs, col_ai = st.columns([2, 4])
+    with col_obs:
+        with st.form(key=f"form_{doc.id}_obs", border=False):
+            user_input = st.text_area(
+                "Observações",
+                height=200,
+                value=st.session_state[f"obs_{doc.id}"]
+                if f"obs_{doc.id}" in st.session_state
+                else "",
+                key=f"obs_{doc.id}",
+            )
+            btn_generate = st.form_submit_button(
+                "Gerar documento",
+                icon=":material/text_fields_alt:",
+            )
+            if btn_generate:
+                if user_input:
+                    ai_content: DocumentContent = await llm.generate_content(
+                        doc, user_input
+                    )
+                    st.session_state[f"ai_{doc.id}"] = ai_content.content
+                    # Optionally append to document content
+                    st.session_state[f"content_{doc.id}"] += "\n\n" + ai_content.content
+
+    with col_ai:
+        st.markdown("Prontuário gerado por IA")
+        if st.session_state[f"ai_{doc.id}"]:
+            st.markdown(st.session_state[f"ai_{doc.id}"])
+
+    with st.form(key=f"form_{doc.id}_content", clear_on_submit=False):
+        doc.content.content = st.text_area(
+            "Prontuário completo",
+            key=f"content_{doc.id}",
+            height=400,
+            value=st.session_state[f"content_{doc.id}"]
+            if f"content_{doc.id}" in st.session_state
+            else doc.content.content,
         )
-    with col_btn:
-        if st.button(
-            "Gerar documento",
-            key=f"AI_{patient_.id}",
-            icon=":material/text_fields_alt:",
-        ):
-            pass
-    with col_output:
-        ai_content = st.text_area("Documento gerado por IA", height=200)
-
-    doc.content += "\n\n" + ai_content  # for prontuaries, have to check the rest
-
-    doc.content = st.text_area(
-        "Resultado",
-        key=f"content_{patient_.id}",
-        height=400,
-        value=doc.content,
-    )
-
-    if st.button("Salvar", icon=":material/save:"):
-        insert_document(doc)
-        st.query_params.pop("edit")
-        st.rerun()
+        btn_save = st.form_submit_button(
+            "Salvar",
+            icon=":material/save:",
+        )
+        if btn_save:
+            insert_document(doc)
+            st.toast("Documento salvo com sucesso!", icon=":material/check:")
 
 
-def display_docs_header(patient_name: str) -> None:
-    st.title(f"Documentos de {patient_name.strip()}")
+def display_docs_header(patient_: Patient) -> None:
+    st.title(f"Documentos de {patient_.name.strip()}")
     cols = st.columns(4)
     with cols[0]:
         if st.button("Voltar para a lista de pacientes", icon=":material/arrow_back:"):
@@ -72,8 +89,9 @@ def display_docs_header(patient_name: str) -> None:
             st.session_state.pop("patient")
             st.rerun()
     with cols[1]:
-        if st.button("Adicionar documento", icon=":material/attach_file:"):
+        if st.button("Adicionar documento", icon=":material/docs:"):
             st.query_params["edit"] = "true"
+            st.session_state["doc"] = Document(patient_id=patient_.id)
             st.rerun()
 
     st.divider()
@@ -83,14 +101,26 @@ def display_patient_docs(patient_id: UUID) -> None:
     st.session_state["patient"] = get_patient_by_id(patient_id)
     patient_ = st.session_state["patient"]
 
-    display_docs_header(patient_name=patient_.name)
+    display_docs_header(patient_)
 
-    for doc_category in DocumentCategory:
+    all_patient_docs: list[Document] = get_all_documents_for(patient_.id)
+
+    if not all_patient_docs:
+        st.info("Nenhum documento encontrado para este paciente.")
+        return
+
+    categories: list[DocumentCategory] = list(
+        np.unique([doc.category for doc in all_patient_docs])
+    )
+    for doc_category in categories:
         with st.expander(f"**{DOCUMENT_CATEGORY_PT[doc_category].capitalize()}**"):
-            docs: list[Document] = get_all_for_category(
-                patient_id=patient_.id, category=doc_category
-            )
+            docs: list[Document] = [
+                doc for doc in all_patient_docs if doc.category == doc_category
+            ]
             if not docs:
                 st.info("Nenhum documento deste tipo encontrado.")
             for doc in docs:
-                st.markdown(f"- {doc.file_name}")
+                if st.button(f"{doc.file_name}", type="tertiary"):
+                    st.query_params["edit"] = "true"
+                    st.session_state["doc"] = doc
+                    st.rerun()
