303cfc62abb229680339da444da6b9363fb37d68
llazzari <lucas.lazzari@outlook.com>
2025-07-21 09:04:34 -0300

    feat: enhance patient data structure by integrating PatientInfo and Child models for improved organization and detail

--- src/data/mock.py
+++ src/data/mock.py
@@ -1,33 +1,57 @@
 from datetime import date, time
 
 from data import appointment, patient
-from data.models import Appointment, AppointmentStatus, Patient, PatientStatus
+from data.models import (
+    Appointment,
+    AppointmentStatus,
+    Child,
+    ClassTime,
+    Patient,
+    PatientInfo,
+    PatientStatus,
+)
 from service.database_manager import get_db_connection
 
 
 def get_patients() -> list[Patient]:
     return [
         Patient(
-            name="João Da Sigolandia Junior",
-            address="Rua 1",
-            contact="123456789",
-            birthdate=date(2022, 1, 1),
-            is_child=True,
-            cpf_cnpj="123.456.789-00",
-            school="Escola 1",
-            tutor_cpf_cnpj="987.654.321-00",
+            info=PatientInfo(
+                name="João Da Silva Junior",
+                address="Rua 1",
+                contact="123456789",
+                birthdate=date(2022, 1, 1),
+                cpf_cnpj="123.456.789-00",
+            ),
             status=PatientStatus.ACTIVE,
+            diagnosis="Diagnóstico 1",
+            contract="Contrato 1",
+            child=Child(
+                school="Escola 1",
+                grade="1º Ano",
+                class_time=ClassTime.MORNING,
+                tutor_name="Tutor 1",
+                tutor_cpf_cnpj="987.654.321-00",
+            ),
         ),
         Patient(
-            name="Maria da Silva Sauro",
-            address="Rua 2",
-            contact="987654321",
-            birthdate=date(2020, 1, 1),
-            is_child=False,
-            cpf_cnpj="987.654.321-00",
-            school="Escola 2",
-            tutor_cpf_cnpj="123.456.789-00",
+            info=PatientInfo(
+                name="Maria da Silva Santos",
+                address="Rua 2",
+                contact="987654321",
+                birthdate=date(2020, 1, 1),
+                cpf_cnpj="987.654.321-00",
+            ),
             status=PatientStatus.IN_TESTING,
+            diagnosis="Diagnóstico 2",
+            contract="Contrato 2",
+            child=Child(
+                school="Escola 2",
+                grade="2º Ano",
+                class_time=ClassTime.AFTERNOON,
+                tutor_name="Tutor 2",
+                tutor_cpf_cnpj="123.456.789-00",
+            ),
         ),
     ]
 
@@ -42,14 +66,14 @@ def get_appointments(patients: list[Patient]) -> list[Appointment]:
     return [
         Appointment(
             patient_id=patients[0].id,
-            patient_name=patients[0].name,
+            patient_name=patients[0].info.name,
             appointment_date=date.today(),
             appointment_time=time(14, 30),
             status=AppointmentStatus.DONE,
         ),
         Appointment(
             patient_id=patients[1].id,
-            patient_name=patients[1].name,
+            patient_name=patients[1].info.name,
             appointment_date=date.today(),
             appointment_time=time(15, 30),
             duration=90,
