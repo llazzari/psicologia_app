8faae79af86782f7283efc4f405953e85098c01c
llazzari <lucas.lazzari@outlook.com>
2025-07-23 09:36:09 -0300

    feat: add file reading utilities for text and PDF documents, enabling flexible content retrieval based on file type

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,5 +1,8 @@
 import calendar
+import pathlib
 from datetime import date, timedelta
+
+import pypdf
 
 
 def get_week_days(base_date: date) -> list[date]:
@@ -26,3 +29,28 @@ def get_last_day_of_month(year: int, month: int) -> date:
     _, num_days = calendar.monthrange(year, month)
     last_day = date(year, month, num_days)
     return last_day
+
+
+def read_text(file_path: pathlib.Path) -> str:
+    return file_path.read_text()
+
+
+def read_pdf(file_path: pathlib.Path) -> str:
+    with file_path.open("rb") as f:
+        reader = pypdf.PdfReader(f)
+        text: str = ""
+        for page in reader.pages:
+            text += page.extract_text() or ""
+        return text
+
+
+def read_file(file_path: pathlib.Path) -> str:
+    """
+    Reads a file based on its extension and returns its content.
+    """
+    if file_path.suffix == ".pdf":
+        return read_pdf(file_path)
+    elif file_path.suffix == ".md":
+        return read_text(file_path)
+    else:
+        raise ValueError(f"Unsupported file type: {file_path.suffix}")
