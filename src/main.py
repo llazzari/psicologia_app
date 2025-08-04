a449a73670064155bc013eeb2843ae0714b82dd2
llazzari <lucas.lazzari@outlook.com>
2025-08-04 11:24:40 -0300

    refactor: comment out user login logic for easier testing

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ from service.database_manager import initialize_database
 
 def main():
     """Main function to run the Streamlit application."""
-
     st.set_page_config(
         page_title="Sistema de Gerenciamento",
         page_icon=":material/network_intelligence:",
@@ -15,19 +14,21 @@ def main():
         initial_sidebar_state="expanded",
     )
 
-    if not st.user.is_logged_in:
-        if st.button("Login com Google", icon=":material/login:"):
-            st.login("google")
-    else:
-        navbar.render()
-        homepage.render()
-        initialize_database()
-        # mock.insert_patients()
-        # mock.insert_appointments()
-        # settings_service.insert(PsychologistSettings(user_email=st.user.email))  # type: ignore
-        # st.session_state.settings = settings_service.get_by_(st.user.email)  # type: ignore
+    # if not st.user.is_logged_in:
+    #     if st.button("Login com Google", icon=":material/login:"):
+    #         st.login("google")
 
-        # llm.get_document_content()
+    # else:
+    navbar.render()
+    homepage.render()
+    initialize_database()
+
+    # mock.insert_patients()
+    # mock.insert_appointments()
+    # settings_service.insert(PsychologistSettings(user_email=st.user.email))  # type: ignore
+    # st.session_state.settings = settings_service.get_by_(st.user.email)  # type: ignore
+
+    # llm.get_document_content()
 
 
 # TODO - add a company page with expenses and revenues
